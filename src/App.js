51e6eef1541fb25b0ee5e385f07f60956b954e16
rishi0w0 <142206252+rishi0w0@users.noreply.github.com>
2024-08-26 16:37:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout';
+import LoadingSpinner from './components/LoadingSpinner';
 
-const App = () => {
+// Lazy load components
+const ChatbotPanel = lazy(() => import('./components/ChatbotPanel'));
+const DataVisualizerPanel = lazy(() => import('./components/DataVisualizerPanel'));
+const EventLoggerPanel = lazy(() => import('./components/EventLoggerPanel'));
+const PuppeteerPanel = lazy(() => import('./components/PuppeteerPanel'));
+
+function App() {
   return (
-    <div className="App">
-      <Layout />
-    </div>
+    <Router>
+      <Layout>
+        <Suspense fallback={<LoadingSpinner />}>
+          <Switch>
+            <Route path="/" exact component={ChatbotPanel} />
+            <Route path="/visualizer" component={DataVisualizerPanel} />
+            <Route path="/logger" component={EventLoggerPanel} />
+            <Route path="/puppeteer" component={PuppeteerPanel} />
+          </Switch>
+        </Suspense>
+      </Layout>
+    </Router>
   );
-};
+}
 
 export default App;
